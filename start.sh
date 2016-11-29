f4dfb5efd4d210621ee49952bb747615b045294a
Zhang Sen <senorsen.zhang@gmail.com>
2016-11-29 16:40:20 +0800

    flag to allow inline c in vcl

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
 varnishlog &
 if [ -z ${VARNISH_CONFIG} ]; then
-  varnishd -F -s ${VARNISH_CACHE} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}
+  varnishd -F -s ${VARNISH_CACHE} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT} -p vcc_allow_inline_c=on
 else
-  varnishd -F -f /etc/varnish/default.vcl -s ${VARNISH_CACHE} -a :80
+  varnishd -F -f /etc/varnish/default.vcl -s ${VARNISH_CACHE} -a :80 -p vcc_allow_inline_c=on
 fi
