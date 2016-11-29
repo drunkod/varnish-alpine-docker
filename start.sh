242d9daa285e589b49f9b831d03ca1ab47d9fbab
Zhang Sen <senorsen.zhang@gmail.com>
2016-11-29 19:46:33 +0800

    performance tuning

--- start.sh
+++ start.sh
@@ -2,8 +2,15 @@
 
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
 varnishlog &
+
+VARNISH_PARAMETERS="-a :80 -p vcc_allow_inline_c=on
+    -p thread_pool_min=200
+    -p thread_pool_max=4000
+    -p thread_pool_add_delay=1
+    -p listen_depth=2048"
+
 if [ -z ${VARNISH_CONFIG} ]; then
-  varnishd -F -s ${VARNISH_CACHE} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT} -p vcc_allow_inline_c=on
+  varnishd -F -s ${VARNISH_CACHE} -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT} ${VARNISH_PARAMETERS}
 else
-  varnishd -F -f /etc/varnish/default.vcl -s ${VARNISH_CACHE} -a :80 -p vcc_allow_inline_c=on
+  varnishd -F -f /etc/varnish/default.vcl -s ${VARNISH_CACHE} ${VARNISH_PARAMETERS}
 fi
